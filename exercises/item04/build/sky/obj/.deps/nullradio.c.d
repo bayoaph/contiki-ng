ccbdfde4ebf83fef81d04912753588ef3533b8fa
Daniel Alexis Cruz <dcruz@up.edu.ph>
2024-11-19 12:57:54 +0800

    Finished item04

---
+++ exercises/item04/build/sky/obj/.deps/nullradio.c.d
@@ -0,0 +1,6 @@
+build/sky/obj/nullradio.o: ../../main_contiki/os/dev/nullradio.c \
+ ../../main_contiki/os/dev/nullradio.h ../../main_contiki/os/dev/radio.h
+
+../../main_contiki/os/dev/nullradio.h:
+
+../../main_contiki/os/dev/radio.h:
