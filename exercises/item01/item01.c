00e5c403d14254f2b4d6e3db14cb5bc480f4beb0
clareenz <mlzerrudo@up.edu.ph>
2024-11-25 11:14:00 +0800

    first commit

--- exercises/item01/item01.c
+++ exercises/item01/item01.c
@@ -21,13 +21,15 @@ PROCESS_THREAD(led_blink_process, ev, data)
 
   while (1)
   {
-    leds_on(LEDS_BLUE);
+    leds_on(LEDS_YELLOW);
     etimer_set(&timer_blink, CLOCK_SECOND * 1);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_blink));
+  
 
-    leds_off(LEDS_BLUE);
+    leds_off(LEDS_YELLOW);
     etimer_set(&timer_wait, CLOCK_SECOND * 3);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+
   }
 
   PROCESS_END();
@@ -51,6 +53,7 @@ PROCESS_THREAD(hello_world_process, ev, data)
   PROCESS_END();
 }
 
+
 // autostart processes (can also be more than one)
 AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
 
@@ -71,17 +74,84 @@ AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
 
 /* Exercise 1b: uncomment the three lines with the timer, compile and flash it to the sensor.
  * Observe what happens
+ * The light starts turning off
+ 
  */
 
 /* Exercise 1c: alter the program such that the node prints "Hello World" infinitely to the screen,
  * with the delay of 4 seconds in between each statement
+ *Changes are below
+  PROCESS_THREAD(hello_world_process, ev, data)
+  {
+    PROCESS_BEGIN();
+    static struct etimer timer_startup, timer_wait;
+
+    etimer_set(&timer_startup, CLOCK_SECOND * 30); // Startup delay
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
+
+    while (1) // Infinite loop
+    {
+      printf("Hello, world\n"); // Print "Hello, world"
+      etimer_set(&timer_wait, CLOCK_SECOND * 4); // 4-second delay
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+    }
+
+    PROCESS_END();
+  }
+
  */
 
-/* Exercise 1d: alter the program such that the node stops printing "Hello World" after 10 times
+/* Exercise 1d: alter the program such that the node stops printing "Hello World" after 10 times'
+  *Changes are below
+  *add a counter and change the condition of the while loop to counter < 10
+  PROCESS_THREAD(hello_world_process, ev, data)
+  {
+    PROCESS_BEGIN();
+    static struct etimer timer_startup, timer_wait;
+    static int counter = 0;
+
+    etimer_set(&timer_startup, CLOCK_SECOND * 30);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
+
+    while (counter < 10)
+    {
+      printf("Hello, world\n");
+      counter++;
+      etimer_set(&timer_wait, CLOCK_SECOND * 4);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+    }
+
+    PROCESS_END();
+  }
  */
 
 /* Exercise 1e: alter the program such that the node turns on the blue led in each loop,
  * keeps it turned on for 1 second and then continues.
+ *Comment out the led blink process and incorporate an led blink in the hello world process
+    PROCESS_THREAD(hello_world_process, ev, data)
+    {
+      PROCESS_BEGIN();
+      static struct etimer timer_startup, timer_wait, led_timer;
+
+      etimer_set(&timer_startup, CLOCK_SECOND * 30);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
+
+      while (1)
+      {
+        printf("Hello, world\n");
+        leds_on(LEDS_YELLOW);
+        etimer_set(&led_timer, CLOCK_SECOND * 1);
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&led_timer));
+        leds_off(LEDS_YELLOW);
+
+        etimer_set(&timer_wait, CLOCK_SECOND * 4);
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+      }
+
+      PROCESS_END();
+    }
+
+
  */
 
 /* Exercise 1f: separate the led blinking logic from the "Hello World"
@@ -91,4 +161,45 @@ AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
  *
  * The two processes shall be started when the node boots. The first (a) shall start immediately,
  * the second (b) shall start 30s after bootup.
+ 
+ *Led Blink Process(a)
+    PROCESS_THREAD(led_blink_process, ev, data)
+    {
+      PROCESS_BEGIN();
+      static struct etimer timer_blink, timer_wait;
+      // Blink pattern every 4 seconds
+
+      while (1)
+      {
+        leds_on(LEDS_YELLOW);
+        etimer_set(&timer_blink, CLOCK_SECOND * 1);
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_blink));
+      
+
+        leds_off(LEDS_YELLOW);
+        etimer_set(&timer_wait, CLOCK_SECOND * 3);
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+
+      }
+
+      PROCESS_END();
+    }
+  *Hello World Process(b)
+    PROCESS_THREAD(hello_world_process, ev, data)
+    {
+      PROCESS_BEGIN();
+      static struct etimer timer_startup, timer_wait;
+
+      etimer_set(&timer_startup, CLOCK_SECOND * 30); // After 30 seconds Hello World stars printing
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
+
+      while (1)
+      {
+        printf("Hello, world\n");
+        etimer_set(&timer_wait, CLOCK_SECOND * 10); //Every 10 seconds Hello World Prints
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+      }
+
+      PROCESS_END();
+    }
  */
