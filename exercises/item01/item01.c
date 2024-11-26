4312b5cbaf9522dcea307491c1f0082ca1146300
KCerio <ceriokristine@gmail.com>
2024-11-26 10:39:54 +0800

    exercise 1-3

--- exercises/item01/item01.c
+++ exercises/item01/item01.c
@@ -16,16 +16,17 @@ PROCESS(hello_world_process, "Hello World Timer Process");
 PROCESS_THREAD(led_blink_process, ev, data)
 {
   PROCESS_BEGIN();
-  static struct etimer timer_blink, timer_wait;
+  static struct etimer timer_blink, timer_wait; // comment timer_wait in doing exercise 1b
   // Blink pattern every 4 seconds
 
   while (1)
   {
-    leds_on(LEDS_BLUE);
+    leds_on(LEDS_YELLOW);
     etimer_set(&timer_blink, CLOCK_SECOND * 1);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_blink));
 
-    leds_off(LEDS_BLUE);
+    // uncomment
+    leds_off(LEDS_YELLOW);
     etimer_set(&timer_wait, CLOCK_SECOND * 3);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
   }
@@ -37,22 +38,38 @@ PROCESS_THREAD(hello_world_process, ev, data)
 {
   PROCESS_BEGIN();
   static struct etimer timer_startup, timer_wait;
+  static int count = 1;
 
   etimer_set(&timer_startup, CLOCK_SECOND * 30);
   PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
 
   while (1)
   {
-    printf("Hello, world\n");
-    etimer_set(&timer_wait, CLOCK_SECOND * 10);
+    //exercise 1e
+    leds_on(LEDS_GREEN);
+    etimer_set(&timer_wait, CLOCK_SECOND * 1);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
-  }
+    leds_off(LEDS_GREEN);
 
+    printf("%d\n", count);
+    printf("Hello, world\n");
+    etimer_set(&timer_wait, CLOCK_SECOND * 4); // exercise C: change 10 to 4
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+    count++;
+    
+
+    // exercise 1d
+    // count++;
+    if (count > 10) {
+      break;
+    }
+  }
   PROCESS_END();
 }
 
 // autostart processes (can also be more than one)
 AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
+
 
 /* Exercise 1a: compile the Contiki OS and flash the node attached to the PC via USB with
  * the above process it usually works best with flashing when you are the superuser
@@ -69,26 +86,154 @@ AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
  * "Hello World" printed onto the screen
  */
 
+
+/*
+  Answer Exercise 1a:
+
+*/
+
+
 /* Exercise 1b: uncomment the three lines with the timer, compile and flash it to the sensor.
  * Observe what happens
- */
+ 
+  Answer Exercise 1b:
+
+  Lines to uncomment on led_blink_process thread:
+    leds_off(LEDS_YELLOW);
+    etimer_set(&timer_wait, CLOCK_SECOND * 3);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+
+  If we uncomment the 3 lines with the timer, the LED turns off for 3 seconds. This 3 lines makes the LED blink. 
+  If we comment it back, the LED would continue to turn on until the program is stop. 
+*/
 
 /* Exercise 1c: alter the program such that the node prints "Hello World" infinitely to the screen,
  * with the delay of 4 seconds in between each statement
+
+  Answer Exercise 1c:
+  Under the hello_world_process thread, change the delay to 4 seconds. With this, the interval of printing is 4 seconds instead of 10 seconds.
+
+  Modification:
+    while (1)
+    {
+      printf("Hello, world\n");
+      etimer_set(&timer_wait, CLOCK_SECOND * 4); // from 10 sec to 4 sec
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+    }
  */
 
 /* Exercise 1d: alter the program such that the node stops printing "Hello World" after 10 times
+
+  Answer Exercise 1d:
+
+  Modification on the while loop under the hello_world_process thread:
+    while (1)
+    {
+      printf("%d\n", count); // debug purposes
+      
+      printf("Hello, world\n");
+      etimer_set(&timer_wait, CLOCK_SECOND * 4); // exercise C: change 10 to 4
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+    
+      // exercise 1d
+      count++;
+      if (count > 10) {
+        break;
+      }
+    }
+ 
+    By modifying the while loop, the program will only continue to print "Hello World" until count reaches 10. 
+    Count was initialized to 1 at the beginning of the thread.
  */
 
 /* Exercise 1e: alter the program such that the node turns on the blue led in each loop,
  * keeps it turned on for 1 second and then continues.
+
+  Answer Exercise 1e:
+  Modification on the while loop under the hello_world_process thread:
+    while (1)
+    {
+      //exercise 1e
+      leds_on(LEDS_GREEN);
+      etimer_set(&timer_wait, CLOCK_SECOND * 1);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+      leds_off(LEDS_GREEN);
+
+      printf("Hello, world\n");
+      etimer_set(&timer_wait, CLOCK_SECOND * 4); // exercise C: change 10 to 4
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+      }
+    }
+
+    The modification turns on the Green LED for 1 second and then turns it off afterward, while printing the "Hello World". 
+    This continues until the loop or program is stopped. 
  */
 
 /* Exercise 1f: separate the led blinking logic from the "Hello World"
- * printing logic: use two different processes, one that a) infinitely turns on and off the blue
- * LED (blink time = 1s, repeat interval=4s) and one that b) infinitely prints "Hello World"
+ * printing logic: use two different processes, one that 
+ a) infinitely turns on and off the blue LED (blink time = 1s, repeat interval=4s) 
+ b) infinitely prints "Hello World"
  * with an interval of 10s
  *
  * The two processes shall be started when the node boots. The first (a) shall start immediately,
  * the second (b) shall start 30s after bootup.
+ * 
+ * Answer:
+ * a. 
+ * PROCESS_THREAD(led_blink_process, ev, data)
+  {
+    PROCESS_BEGIN();
+    static struct etimer timer_blink, timer_wait; // comment timer_wait in doing exercise 1b
+    // Blink pattern every 4 seconds
+
+    while (1)
+    {
+      leds_on(LEDS_YELLOW);
+      etimer_set(&timer_blink, CLOCK_SECOND * 1);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_blink));
+
+      // uncomment
+      leds_off(LEDS_YELLOW);
+      etimer_set(&timer_wait, CLOCK_SECOND * 3);  // 3-second wait to complete 4-second interval
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+    }
+
+    PROCESS_END();
+  }
+
+    In here, the first process thread turns on the yellow LED for 1 second and then turns it off to wait 
+    for 3 seconds before turning the LED on again. The 1 second plus the 3 seconds completes the 
+    4-second interval. This process is repeatedly done in loop until the simulation is stopped or
+    terminated.
+
+   b.
+   PROCESS_THREAD(hello_world_process, ev, data)
+  {
+    PROCESS_BEGIN();
+    static struct etimer timer_startup, timer_wait;
+
+    etimer_set(&timer_startup, CLOCK_SECOND * 30);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
+
+    while (1)
+    {
+      printf("Hello, world\n");
+      etimer_set(&timer_wait, CLOCK_SECOND * 10); 
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+    }
+    PROCESS_END();
+  }
+ AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process); 
+  // auto-starts the two processes a and b but b starts 30 secs after because of the 
+  // 30-sec startup timer 
+
+    In here, the second process thread involves printing "Hello, world" infinitely with 
+    an interval of 10 seconds. Athough this process thread also starts automatically at 
+    the start of the simulation, it only starts printing the string after 30 secs because 
+    of the 30-second timer before the while loop. Hence, it has to wait for the timer to
+    finish before printing. 
+
+
+
+ *  
  */
--- exercises/item03/item03.c
+++ exercises/item03/item03.c
@@ -25,7 +25,7 @@ static struct ctimer leds_off_timer_send;
 /* Timer callback turns off the blue led */
 static void timerCallback_turnOffLeds()
 {
-  leds_off(LEDS_BLUE);
+  leds_off(LEDS_YELLOW);
 }
 
 static void send_broadcast_packet(struct temperatureMessage *payload)
@@ -47,6 +47,12 @@ static void udp_recv_callback(struct simple_udp_connection *c, const uip_ipaddr_
   memcpy(&received_data, data, sizeof(struct temperatureMessage)); // Deserialize
 
   printf("Received Sensor Data: Message = %s, Temperature = %u\n", received_data.messageString, received_data.temperature);
+
+  //Exercise 3b
+  // Blink the blue LED for 1 second
+  leds_on(LEDS_YELLOW);
+  ctimer_set(&leds_off_timer_send, CLOCK_SECOND, timerCallback_turnOffLeds, NULL);
+
 }
 
 /*---------------------------------------------------------------------------*/
@@ -92,7 +98,7 @@ PROCESS_THREAD(broadcast_rssi_process, ev, data)
     send_broadcast_packet(&tm);
 
     /* turn on blue led */
-    leds_on(LEDS_BLUE);
+    leds_on(LEDS_YELLOW);
     /* set the timer "leds_off_timer" to 1 second */
     ctimer_set(&leds_off_timer_send, CLOCK_SECOND, timerCallback_turnOffLeds, NULL);
     printf("sent broadcast message\n");
@@ -103,8 +109,17 @@ PROCESS_THREAD(broadcast_rssi_process, ev, data)
   PROCESS_END();
 }
 
-/* Exercise 3a: flash the program to your nodes and observe what happens. Read the code and understand it thoroughly.
- */
+/**Exercise 3a: flash the program to your nodes and observe what happens. Read the code and understand it thoroughly.
+ *
+ * My Answer:
+ * This program implements a simple broadcast system using contiki OS and simple-udp module. The program implements a 
+ * UDP broadcast where nodes with temperature sensors send their temperature readings to all other nodes, while nodes 
+ * without sensors send a message indicating their lack of a sensor. The broadcasting nodes are the once who sends the 
+ * current temperature value and a meassage to other node every 4 seconds. While the receiving nodes receive the message
+ * and temperature to the serial console and blink the LED for 1 second. However, those nodes that do not have sensors
+ * will send a message indicating that the do not have a tenperature sensor. 
+ *  
+ */ 
 
 /* Exercise 3b (to be solved in ILIAS): for the node with the temperature sensor, read out the temperature value
  * in each loop. Then, send it to the other node using the broadcast function. Use the defined struct "temperatureMessage".
@@ -122,5 +137,119 @@ PROCESS_THREAD(broadcast_rssi_process, ev, data)
  * When received by the other node, let the receiver print the transmitted temperature to the serial port
  * AND let it blink with the blue LED for 1 second!
  *
- *
- */
+ * 
+ * My Answer:
+ * In here, the nodes with temperature sensors (even nodes) reads the temperature and sends it to other nodes by broadcasting
+ * it. The message is package in temperatureMessage data struct and is transmitted. Once the other nodes recceives the
+ * message, they print the temperature and blink the LED for 1 second.
+ * 
+ * Modified Program:*/ 
+// #include "contiki.h"
+// #include <stdio.h> /* For printf() */
+// #include "etimer.h"
+// #include <string.h>
+// #include "dev/button-sensor.h"
+// #include "dev/leds.h"
+// #include "node-id.h" /* get a pointer to the own node id */
+// #include "dev/zoul-sensors.h"
+// #include "simple-udp.h"
+
+// #define UDP_PORT 1234
+// #define BROADCAST_ADDR "ff02::1" // Link-local all nodes multicast address
+
+// struct temperatureMessage
+// {
+//   char messageString[50];
+//   uint16_t temperature;
+// };
+
+// static struct simple_udp_connection udp_conn;
+// static struct ctimer leds_off_timer_send;
+
+// /* Timer callback turns off the blue led */
+// static void timerCallback_turnOffLeds()
+// {
+//   leds_off(LEDS_YELLOW);
+// }
+
+// static void send_broadcast_packet(struct temperatureMessage *payload)
+// {
+//   uip_ipaddr_t broadcast_addr;
+
+//   // Create a buffer to hold the serialized data
+//   uint8_t buffer[sizeof(struct temperatureMessage)];
+//   memcpy(buffer, payload, sizeof(struct temperatureMessage)); // Serialize
+
+//   uiplib_ipaddrconv(BROADCAST_ADDR, &broadcast_addr);
+//   simple_udp_sendto(&udp_conn, buffer, strlen(buffer), &broadcast_addr);
+// }
+
+// // Callback function to handle incoming packets
+// static void udp_recv_callback(struct simple_udp_connection *c, const uip_ipaddr_t *sender_addr, const uint8_t *data, uint16_t datalen)
+// {
+//   struct temperatureMessage received_data;
+//   memcpy(&received_data, data, sizeof(struct temperatureMessage)); // Deserialize
+
+//   printf("Received Sensor Data: Message = %s, Temperature = %u\n", received_data.messageString, received_data.temperature);
+
+//   //Exercise 3b
+//   // Blink the blue LED for 1 second
+//   leds_on(LEDS_YELLOW);
+//   ctimer_set(&leds_off_timer_send, CLOCK_SECOND, timerCallback_turnOffLeds, NULL);
+
+// }
+
+// /*---------------------------------------------------------------------------*/
+// /* one timer struct declaration/instantiation */
+// static struct etimer et;
+// /* one temperatureMessage struct declaration/instantiation  */
+// static struct temperatureMessage tm;
+// /*---------------------------------------------------------------------------*/
+// PROCESS(broadcast_rssi_process, "Broadcast RSSI");
+// AUTOSTART_PROCESSES(&broadcast_rssi_process);
+// /*---------------------------------------------------------------------------*/
+// PROCESS_THREAD(broadcast_rssi_process, ev, data)
+// {
+//   // PROCESS_EXITHANDLER(broadcast_close(&bc);)
+//   PROCESS_BEGIN();
+
+//   SENSORS_ACTIVATE(cc2538_temp_sensor);
+
+//   printf("Starting broadcast\n");
+//   simple_udp_register(&udp_conn, UDP_PORT, NULL, UDP_PORT, udp_recv_callback)
+
+//       /* Broadcast every 4 seconds */
+//       etimer_set(&et, 4 * CLOCK_SECOND);
+
+//   while (1)
+//   {
+//     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+//     etimer_reset(&et);
+
+//     // Temperature of 0 means no sensor, checked using node_id an even number
+//     uint16_t temperature = (node_id % 2 == 0) ? cc2538_temp_sensor.value(CC2538_SENSORS_VALUE_TYPE_CONVERTED) : 0;
+
+//     // Send a different message, depending on the existence of a temperature sensor
+//     if (node_id % 2 == 0)
+//       sprintf(tm.messageString, "Here is node %u, sending the temperature", node_id);
+//     else
+//       sprintf(tm.messageString, "Here is node %u, I don't have a sensor", node_id);
+
+//     // Save the temperature into the struct
+//     tm.temperature = temperature;
+
+//     /* send the packet */
+//     send_broadcast_packet(&tm);
+
+//     /* turn on blue led */
+//     leds_on(LEDS_YELLOW);
+//     /* set the timer "leds_off_timer" to 1 second */
+//     ctimer_set(&leds_off_timer_send, CLOCK_SECOND, timerCallback_turnOffLeds, NULL);
+//     printf("sent broadcast message\n");
+//   }
+
+//   SENSORS_DEACTIVATE(cc2538_temp_sensor);
+
+//   PROCESS_END();
+// }
+ 
