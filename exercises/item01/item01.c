58b7b52f67291800d1827f6d3979a1f3bbd09438
Liam Iñaki Gillamac <yakogillamac96@gmail.com>
2024-12-14 01:13:32 +0800

    Update Exercise 1 output

--- exercises/item01/item01.c
+++ exercises/item01/item01.c
@@ -79,23 +79,88 @@ AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
  * "Hello World" printed onto the screen
  */
 
+
+
 /* Exercise 1b: uncomment the three lines with the timer, compile and flash it to the sensor.
  * Observe what happens
  */
 
-/* OBSERVATION:
- * Uncommenting the three lines in led_blink_process, specifically the part where we turn off the led,
- * makes the led turn on non-stop. Although we needed to remove the declared timer_wait because 
- * it was and unused at the time and causes compilation errors.
- * 
- */
+/*---------------------------------------------------------------------------
+
+OBSERVATION:
+Uncommenting the three lines in led_blink_process, specifically the part where we turn off the led,
+makes the led turn on non-stop. Although we needed to remove the declared timer_wait because 
+it was and unused at the time and causes compilation errors.
+ 
+---------------------------------------------------------------------------*/
+
+
 
 /* Exercise 1c: alter the program such that the node prints "Hello World" infinitely to the screen,
  * with the delay of 4 seconds in between each statement
  */
 
+/*---------------------------------------------------------------------------
+
+PROCESS_THREAD(hello_world_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  static struct etimer timer_startup, timer_wait;
+  etimer_set(&timer_startup, CLOCK_SECOND * 30);
+  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
+
+  while (1)
+  { 
+
+    printf("Hello, world\n");
+
+    etimer_set(&timer_wait, CLOCK_SECOND * 4); // Changing value from 10 to 4 changes delay interval
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+  }
+
+  PROCESS_END();
+}
+
+---------------------------------------------------------------------------*/
+
+
+
+
 /* Exercise 1d: alter the program such that the node stops printing "Hello World" after 10 times
  */
+
+/*---------------------------------------------------------------------------
+
+PROCESS_THREAD(hello_world_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  static int counter = 0; // Track of the number of "Hello, world" prints (Used in Exercise 1d)
+
+  static struct etimer timer_startup, timer_wait;
+  etimer_set(&timer_startup, CLOCK_SECOND * 30);
+  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
+
+  while (1)
+  { 
+    // Exercise 1d: Stops after 10 prints
+    if (counter >= 10) 
+      break;
+
+    printf("Hello, world\n");
+    counter++; (Used in Exercise 1d) Increment number of prints
+
+    etimer_set(&timer_wait, CLOCK_SECOND * 10);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+  }
+
+  PROCESS_END();
+}
+
+---------------------------------------------------------------------------*/
+
+
 
 /* Exercise 1e: alter the program such that the node turns on the blue led in each loop,
  * keeps it turned on for 1 second and then continues.
@@ -105,6 +170,8 @@ AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
  * For some reason, there are issues on using blue led.
  * Hence, red led was used as substitute, since it works well.
  */
+
+
 
 /* Exercise 1f: separate the led blinking logic from the "Hello World"
  * printing logic: use two different processes, one that a) infinitely turns on and off the blue
