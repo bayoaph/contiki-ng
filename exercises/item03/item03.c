ff2d3f5eb092d7061a88bf4d1865a9700b9ba08b
jlmejia1 <123083662+jlmejia1@users.noreply.github.com>
2024-12-13 17:26:24 +0800

    Answers to Exercise 3
    Signed-off-by: jlmejia1 <123083662+jlmejia1@users.noreply.github.com>

--- exercises/item03/item03.c
+++ exercises/item03/item03.c
@@ -1,14 +1,19 @@
+/* Philipp Hurni, University of Bern, December 2013     */
+/* Modified for Zolertia Firefly and Contiki-NG - M. Cabilo, Apr 2019  */
+
 /* Based on exercises by Philipp Hurni, University of Bern, December 2013 */
 
 #include "contiki.h"
 #include <stdio.h> /* For printf() */
-#include "etimer.h"
+#include "sys/etimer.h"
 #include <string.h>
 #include "dev/button-sensor.h"
 #include "dev/leds.h"
-#include "node-id.h" /* get a pointer to the own node id */
-#include "dev/zoul-sensors.h"
-#include "simple-udp.h"
+#include "sys/node-id.h" /* get a pointer to the own node id */
+//#include "dev/zoul-sensors.h"
+#include "dev/sht11/sht11-sensor.h"
+#include "net/ipv6/simple-udp.h"
+#include "net/ipv6/uiplib.h"
 
 #define UDP_PORT 1234
 #define BROADCAST_ADDR "ff02::1" // Link-local all nodes multicast address
@@ -30,23 +35,27 @@ static void timerCallback_turnOffLeds()
 
 static void send_broadcast_packet(struct temperatureMessage *payload)
 {
-  uip_ipaddr_t broadcast_addr;
-
-  // Create a buffer to hold the serialized data
-  uint8_t buffer[sizeof(struct temperatureMessage)];
-  memcpy(buffer, payload, sizeof(struct temperatureMessage)); // Serialize
-
-  uiplib_ipaddrconv(BROADCAST_ADDR, &broadcast_addr);
-  simple_udp_sendto(&udp_conn, buffer, strlen(buffer), &broadcast_addr);
+    uip_ipaddr_t broadcast_addr;
+    uint8_t buffer[sizeof(struct temperatureMessage)];
+    memcpy(buffer, payload, sizeof(struct temperatureMessage)); // Serialize
+    uiplib_ipaddrconv(BROADCAST_ADDR, &broadcast_addr);
+    simple_udp_sendto(&udp_conn, buffer, sizeof(buffer), &broadcast_addr); // Use sizeof instead of strlen
 }
 
 // Callback function to handle incoming packets
-static void udp_recv_callback(struct simple_udp_connection *c, const uip_ipaddr_t *sender_addr, const uint8_t *data, uint16_t datalen)
+static void udp_recv_callback(struct simple_udp_connection *c, 
+                              const uip_ipaddr_t *sender_addr, 
+                              uint16_t sender_port, 
+                              const uip_ipaddr_t *receiver_addr, 
+                              uint16_t receiver_port, 
+                              const uint8_t *data, 
+                              uint16_t datalen)
 {
-  struct temperatureMessage received_data;
-  memcpy(&received_data, data, sizeof(struct temperatureMessage)); // Deserialize
-
-  printf("Received Sensor Data: Message = %s, Temperature = %u\n", received_data.messageString, received_data.temperature);
+    struct temperatureMessage received_data;
+    memcpy(&received_data, data, sizeof(struct temperatureMessage)); // Deserialize
+    printf("Received Sensor Data: Message = %s, Temperature = %u\n", 
+           received_data.messageString, 
+           received_data.temperature);
 }
 
 /*---------------------------------------------------------------------------*/
@@ -63,13 +72,13 @@ PROCESS_THREAD(broadcast_rssi_process, ev, data)
   // PROCESS_EXITHANDLER(broadcast_close(&bc);)
   PROCESS_BEGIN();
 
-  SENSORS_ACTIVATE(cc2538_temp_sensor);
+  SENSORS_ACTIVATE(sht11_sensor);
 
   printf("Starting broadcast\n");
-  simple_udp_register(&udp_conn, UDP_PORT, NULL, UDP_PORT, udp_recv_callback)
+  simple_udp_register(&udp_conn, UDP_PORT, NULL, UDP_PORT, udp_recv_callback);
 
-      /* Broadcast every 4 seconds */
-      etimer_set(&et, 4 * CLOCK_SECOND);
+  /* Broadcast every 4 seconds */
+  etimer_set(&et, 4 * CLOCK_SECOND);
 
   while (1)
   {
@@ -77,7 +86,7 @@ PROCESS_THREAD(broadcast_rssi_process, ev, data)
     etimer_reset(&et);
 
     // Temperature of 0 means no sensor, checked using node_id an even number
-    uint16_t temperature = (node_id % 2 == 0) ? cc2538_temp_sensor.value(CC2538_SENSORS_VALUE_TYPE_CONVERTED) : 0;
+    uint16_t temperature = (node_id % 2 == 0) ? sht11_sensor.value(SHT11_SENSOR_TEMP) : 0;
 
     // Send a different message, depending on the existence of a temperature sensor
     if (node_id % 2 == 0)
@@ -98,10 +107,12 @@ PROCESS_THREAD(broadcast_rssi_process, ev, data)
     printf("sent broadcast message\n");
   }
 
-  SENSORS_DEACTIVATE(cc2538_temp_sensor);
+  SENSORS_DEACTIVATE(sht11_sensor);
 
   PROCESS_END();
 }
+
+
 
 /* Exercise 3a: flash the program to your nodes and observe what happens. Read the code and understand it thoroughly.
  */
@@ -118,6 +129,28 @@ PROCESS_THREAD(broadcast_rssi_process, ev, data)
  *
  * containing the string (messageString) and the current temperature (temperature).
  * Attention: watch out how you write into the temperatureMessage.messageString field in the C programming language...
+ *
+ * When received by the other node, let the receiver print the transmitted temperature to the serial port
+ * AND let it blink with the blue LED for 1 second!
+ *
+ *
+ */
+
+/* Exercise 3a: flash the program to your nodes and observe what happens. Read the code and understand it thoroughly.
+ */
+
+/* Exercise 3b (to be solved in ILIAS): for the node with the temperature sensor, read out the temperature value
+ * in each loop. Then, send it to the other node using the broadcast function. Use the defined struct "temperatureMessage".
+ * Learn how to define and instantiate structs in C on the internet. Replace the payload "helloMessage" entirely with
+ * an instance of the struct temperatureMessage defined on line 11
+ *
+ * struct temperatureMessage {
+ *	char messageString[50];
+ *	uint16_t temperature;
+ * };
+ *
+ * containing the string (messageString) and the current temperature (temperature).
+ * Attention: watch out how you w0rite into the temperatureMessage.messageString field in the C programming language...
  *
  * When received by the other node, let the receiver print the transmitted temperature to the serial port
  * AND let it blink with the blue LED for 1 second!
---
+++ exercises/item03/sim_item03.csc
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<simconf version="2023090101">
+  <simulation>
+    <title>My simulation</title>
+    <speedlimit>1.0</speedlimit>
+    <randomseed>123456</randomseed>
+    <motedelay_us>1000000</motedelay_us>
+    <radiomedium>
+      org.contikios.cooja.radiomediums.UDGM
+      <transmitting_range>50.0</transmitting_range>
+      <interference_range>100.0</interference_range>
+      <success_ratio_tx>1.0</success_ratio_tx>
+      <success_ratio_rx>1.0</success_ratio_rx>
+    </radiomedium>
+    <events>
+      <logoutput>40000</logoutput>
+    </events>
+    <motetype>
+      org.contikios.cooja.mspmote.SkyMoteType
+      <description>Sky Mote Type #1</description>
+      <source>[CONFIG_DIR]/item03.c</source>
+      <commands>$(MAKE) -j$(CPUS) item03.sky TARGET=sky</commands>
+      <firmware>[CONFIG_DIR]/build/sky/item03.sky</firmware>
+      <moteinterface>org.contikios.cooja.interfaces.Position</moteinterface>
+      <moteinterface>org.contikios.cooja.interfaces.IPAddress</moteinterface>
+      <moteinterface>org.contikios.cooja.interfaces.Mote2MoteRelations</moteinterface>
+      <moteinterface>org.contikios.cooja.interfaces.MoteAttributes</moteinterface>
+      <moteinterface>org.contikios.cooja.mspmote.interfaces.MspClock</moteinterface>
+      <moteinterface>org.contikios.cooja.mspmote.interfaces.MspMoteID</moteinterface>
+      <moteinterface>org.contikios.cooja.mspmote.interfaces.SkyButton</moteinterface>
+      <moteinterface>org.contikios.cooja.mspmote.interfaces.SkyFlash</moteinterface>
+      <moteinterface>org.contikios.cooja.mspmote.interfaces.SkyCoffeeFilesystem</moteinterface>
+      <moteinterface>org.contikios.cooja.mspmote.interfaces.Msp802154Radio</moteinterface>
+      <moteinterface>org.contikios.cooja.mspmote.interfaces.MspSerial</moteinterface>
+      <moteinterface>org.contikios.cooja.mspmote.interfaces.MspLED</moteinterface>
+      <moteinterface>org.contikios.cooja.mspmote.interfaces.MspDebugOutput</moteinterface>
+      <moteinterface>org.contikios.cooja.mspmote.interfaces.SkyTemperature</moteinterface>
+      <mote>
+        <interface_config>
+          org.contikios.cooja.interfaces.Position
+          <pos x="48.24418763686413" y="6.943769416890344" />
+        </interface_config>
+        <interface_config>
+          org.contikios.cooja.mspmote.interfaces.MspMoteID
+          <id>1</id>
+        </interface_config>
+      </mote>
+      <mote>
+        <interface_config>
+          org.contikios.cooja.interfaces.Position
+          <pos x="75.08722748766588" y="75.15941669918843" />
+        </interface_config>
+        <interface_config>
+          org.contikios.cooja.mspmote.interfaces.MspMoteID
+          <id>2</id>
+        </interface_config>
+      </mote>
+      <mote>
+        <interface_config>
+          org.contikios.cooja.interfaces.Position
+          <pos x="22.907305938844623" y="20.88387371790652" />
+        </interface_config>
+        <interface_config>
+          org.contikios.cooja.mspmote.interfaces.MspMoteID
+          <id>3</id>
+        </interface_config>
+      </mote>
+      <mote>
+        <interface_config>
+          org.contikios.cooja.interfaces.Position
+          <pos x="74.87556181431503" y="30.32363010904664" />
+        </interface_config>
+        <interface_config>
+          org.contikios.cooja.mspmote.interfaces.MspMoteID
+          <id>4</id>
+        </interface_config>
+      </mote>
+    </motetype>
+  </simulation>
+  <plugin>
+    org.contikios.cooja.plugins.Visualizer
+    <plugin_config>
+      <moterelations>true</moterelations>
+      <skin>org.contikios.cooja.plugins.skins.IDVisualizerSkin</skin>
+      <skin>org.contikios.cooja.plugins.skins.GridVisualizerSkin</skin>
+      <skin>org.contikios.cooja.plugins.skins.TrafficVisualizerSkin</skin>
+      <skin>org.contikios.cooja.plugins.skins.UDGMVisualizerSkin</skin>
+      <viewport>4.611045516342683 0.0 0.0 4.611045516342683 -31.928626991202233 -16.29076410899694</viewport>
+    </plugin_config>
+    <bounds x="1" y="1" height="400" width="400" />
+  </plugin>
+  <plugin>
+    org.contikios.cooja.plugins.LogListener
+    <plugin_config>
+      <filter />
+      <formatted_time />
+      <coloring />
+    </plugin_config>
+    <bounds x="400" y="160" height="240" width="754" z="1" />
+  </plugin>
+  <plugin>
+    org.contikios.cooja.plugins.TimeLine
+    <plugin_config>
+      <mote>0</mote>
+      <mote>1</mote>
+      <mote>2</mote>
+      <mote>3</mote>
+      <showRadioRXTX />
+      <showRadioHW />
+      <showLEDs />
+      <zoomfactor>500.0</zoomfactor>
+    </plugin_config>
+    <bounds x="0" y="471" height="166" width="1154" z="3" />
+  </plugin>
+  <plugin>
+    org.contikios.cooja.plugins.Notes
+    <plugin_config>
+      <notes>Enter notes here</notes>
+      <decorations>true</decorations>
+    </plugin_config>
+    <bounds x="400" y="0" height="160" width="754" z="2" />
+  </plugin>
+</simconf>
