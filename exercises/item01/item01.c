4c2c981fc9a44f8a7333fb34102212fc573261b4
Shaira Macale <119414029+howtodoitpleasehelp@users.noreply.github.com>
2024-12-15 02:41:48 +0800

    Add files via upload
    Signed-off-by: Shaira Macale <119414029+howtodoitpleasehelp@users.noreply.github.com>

--- exercises/item01/item01.c
+++ exercises/item01/item01.c
@@ -1,12 +1,11 @@
-
-/* Philipp Hurni, University of Bern, December 2013 	 */
+/* Philipp Hurni, University of Bern, December 2013 */
 /* Modified for Zolertia Firefly - M. Cabilo, Apr 2019 */
 
 #include "contiki.h"
 #include "etimer.h"
 #include <stdio.h>
 #include <string.h>
-#include "node-id.h" /* get the variable node_id that holds the own node id */
+// #include "node-id.h" /* get the variable node_id that holds the own node id */
 #include "dev/leds.h"
 /*---------------------------------------------------------------------------*/
 PROCESS(led_blink_process, "LED Blink Timer Process");
@@ -17,16 +16,15 @@ PROCESS_THREAD(led_blink_process, ev, data)
 {
   PROCESS_BEGIN();
   static struct etimer timer_blink, timer_wait;
-  // Blink pattern every 4 seconds
 
   while (1)
   {
-    leds_on(LEDS_BLUE);
+    leds_on(LEDS_RED); //made this red instead of blue as it is not showing in cooja
     etimer_set(&timer_blink, CLOCK_SECOND * 1);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_blink));
 
-    leds_off(LEDS_BLUE);
-    etimer_set(&timer_wait, CLOCK_SECOND * 3);
+    leds_off(LEDS_RED); //made this red instead of blue as it is not showing in cooja
+    etimer_set(&timer_wait, CLOCK_SECOND * 4);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
   }
 
@@ -44,6 +42,7 @@ PROCESS_THREAD(hello_world_process, ev, data)
   while (1)
   {
     printf("Hello, world\n");
+   
     etimer_set(&timer_wait, CLOCK_SECOND * 10);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
   }
@@ -51,7 +50,7 @@ PROCESS_THREAD(hello_world_process, ev, data)
   PROCESS_END();
 }
 
-// autostart processes (can also be more than one)
+// autostart processes (only hello_world_process for testing)
 AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
 
 /* Exercise 1a: compile the Contiki OS and flash the node attached to the PC via USB with
@@ -72,17 +71,91 @@ AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
 /* Exercise 1b: uncomment the three lines with the timer, compile and flash it to the sensor.
  * Observe what happens
  */
+// The yellow LED on the node blinks in a pattern where it turns on for 1 second then turns off for 3 seconds.
 
 /* Exercise 1c: alter the program such that the node prints "Hello World" infinitely to the screen,
  * with the delay of 4 seconds in between each statement
  */
+ /* Changes
+ PROCESS_THREAD(hello_world_process, ev, data)
+{
+  PROCESS_BEGIN();
+  static struct etimer, timer_startup, timer_wait;
+  
+  etimer_set(&timer_startup, CLOCK_SECOND * 30);
+  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
+
+  while (1) // Infinite loop for continuous "Hello World" printing
+  {
+    printf("Hello, world\n");
+    
+    etimer_set(&timer_wait, CLOCK_SECOND * 4); 
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+  }
+
+  PROCESS_END();
+}
+  */
 
 /* Exercise 1d: alter the program such that the node stops printing "Hello World" after 10 times
  */
+/* Changes:
+ PROCESS_THREAD(hello_world_process, ev, data)
+{
+  PROCESS_BEGIN();
+  static struct etimer, timer_startup, timer_wait;
+  static int counter=0;
+  
+  etimer_set(&timer_startup, CLOCK_SECOND * 30);
+  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
+
+  while (counter < 10) // Stops after 10 Hello World prints
+  {
+    printf("Hello, world\n");
+
+    counter++;
+    
+    etimer_set(&timer_wait, CLOCK_SECOND * 4); 
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+  }
+
+  PROCESS_END();
+}
+
+*/
 
 /* Exercise 1e: alter the program such that the node turns on the blue led in each loop,
  * keeps it turned on for 1 second and then continues.
  */
+ /* Changes:
+ PROCESS_THREAD(hello_world_process, ev, data)
+{
+  PROCESS_BEGIN();
+  static struct etimer, timer_startup, timer_blink, timer_wait;
+  static int counter=0;
+  
+  etimer_set(&timer_startup, CLOCK_SECOND * 30);
+  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
+
+  while (counter < 10) // node turns blue after each loop
+  {
+    leds_on(LEDS_BLUE);                   
+    etimer_set(&timer_blink, CLOCK_SECOND * 1); 
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_blink));
+
+    leds_off(LEDS_BLUE);
+    
+    printf("Hello, world\n");
+
+    counter++;
+    
+    etimer_set(&timer_wait, CLOCK_SECOND * 4); 
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+  }
+
+  PROCESS_END();
+} 
+*/
 
 /* Exercise 1f: separate the led blinking logic from the "Hello World"
  * printing logic: use two different processes, one that a) infinitely turns on and off the blue
@@ -92,6 +165,4 @@ AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
  * The two processes shall be started when the node boots. The first (a) shall start immediately,
  * the second (b) shall start 30s after bootup.
  */
-
-/* a comment to commit a crime */
 
