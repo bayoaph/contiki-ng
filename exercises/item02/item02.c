dae7b87da05d3f0fdccbe503227eed3bd1914082
Your Name <nikka.bposeats@gmail.com>
2024-12-14 23:02:01 +0800

    exercise 2

--- exercises/item02/item02.c
+++ exercises/item02/item02.c
@@ -1,40 +1,80 @@
-Answered Exercise Items 1 - 5
-a95251e
-exercises\item02\item02.c
 
-@@ -5,8 +5,9 @@
+/* Philipp Hurni, University of Bern, December 2013     */
+/* Modified for Zolertia Firefly - M. Cabilo, Apr 2019  */
+
 #include <stdio.h> /* For printf() */
-#include <string.h>
-#include "dev/button-sensor.h" /* for the button sensor */
-#include "dev/zoul-sensors.h"  /* for the temperature sensor */
-#include "node-id.h"           /* get the variable node_id that holds the own node id */
-#include "arch/dev/sensor/sht11/sht11-sensor.h"
-//#include "dev/zoul-sensors.h"  /* for the temperature sensor */
-#include "sys/node-id.h"           /* get the variable node_id that holds the own node id */
-#include "dev/leds.h"
+#include "dev/button-sensor.h" /* For the button sensor */
+#include "dev/sht25.h" /* For the temperature sensor */
+#include "dev/leds.h" /* For controlling LEDs */
+#include "sys/etimer.h" /* For event timers */
+#include "sys/process.h" /* For process control */
 /*---------------------------------------------------------------------------*/
 PROCESS(button_press_process, "Button Press Process");
+AUTOSTART_PROCESSES(&button_press_process);
+/*---------------------------------------------------------------------------*/
 
-@@ -22,21 +23,25 @@ PROCESS_THREAD(button_press_process, ev, data)
+/* Helper function to print temperature in float format */
+static void print_temperature(uint16_t temp)
+{
+  printf("Temperature: %u.%uC\n", temp / 1000, temp % 1000);
+}
+
+PROCESS_THREAD(button_press_process, ev, data)
+{
+  static struct etimer blink_timer;
+
   PROCESS_BEGIN();
 
+  /* Activate sensors */
   SENSORS_ACTIVATE(button_sensor);
-  SENSORS_ACTIVATE(sht11_sensor);
+  SENSORS_ACTIVATE(sht25);
 
   while (1)
   {
+    /* Wait for button press event */
     PROCESS_WAIT_EVENT_UNTIL(ev == sensors_event && data == &button_sensor);
 
-    leds_on(LEDS_BLUE);
+    /* Indicate button press */
+    printf("Button pressed\n");
+    leds_on(LEDS_GREEN);
 
-    leds_on(LEDS_RED);
-    printf("button pressed");
-    printf("Temperature: ");
-    SENSORS_ACTIVATE(cc2538_temp_sensor);
-    print_temperature_int_to_float(cc2538_temp_sensor.value(CC2538_SENSORS_VALUE_TYPE_CONVERTED));
-    SENSORS_DEACTIVATE(cc2538_temp_sensor); 
-    print_temperature_int_to_float(sht11_sensor.value(SHT11_SENSOR_TEMP));
+    /* Blink the LED with a 250ms delay */
+    etimer_set(&blink_timer, CLOCK_SECOND / 4);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&blink_timer));
+    leds_off(LEDS_GREEN);
 
-    leds_off(LEDS_BLUE);
-    leds_off(LEDS_RED);
+    /* Read and print temperature */
+    print_temperature(sht25.value(SHT25_VAL_TEMP));
   }
+
+  /* Deactivate sensors (cleanup if process ends) */
+  SENSORS_DEACTIVATE(button_sensor);
+  SENSORS_DEACTIVATE(sht25);
+
+  PROCESS_END();
+}
+
+
+/* Exercise 2a: compile and run the program. press the button, observe what it does with
+ * the serial interface
+ */
+
+// At first, when i compiled the program I am having compilation error. Instead of using ZOUL i opted to use SKY. I installed necessary
+// packages that needs to be installed (msp430-gcc) and edited the MAKEFILE of item 2.
+//When i run the program and pressed the button, the LED also lits up and the prints the temperature that was detected by the sensor.
+
+/* Exercise 2b: alter the program such that whenever the button is pressed, the led
+ * blinks and the string "button pressed" is printed
+ */
+
+// In the code that I have above, I inserted the code printf("button pressed"). This will print "button pressed", everytime i pressed
+// the button of the mote.
+
+/*
+ * Exercise 2c: read out the temperature from the temperature
+ * sensor when the button is pressed. print the temperature to the serial interface by
+ * passing the value read from the sensor to print_temperature_int_to_float().
+ */
+
+//In this exercise, when the button is pressed, the temperature is printed during the print_temperature_int_to_float() function.
+//The printed temperature is in degrees Celcius.
