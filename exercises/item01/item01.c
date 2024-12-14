70763e54a12f33c19d1b13428526cfb65f4683b8
Lably <lcgucor@up.edu.ph>
2024-12-14 22:16:12 +0800

    Update item01 and item02 files

--- exercises/item01/item01.c
+++ exercises/item01/item01.c
@@ -21,11 +21,12 @@ PROCESS_THREAD(led_blink_process, ev, data)
 
   while (1)
   {
-    leds_on(LEDS_BLUE);
+    leds_on(LEDS_RED);
     etimer_set(&timer_blink, CLOCK_SECOND * 1);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_blink));
+    leds_off(LEDS_RED);
 
-    leds_off(LEDS_BLUE);
+    leds_off(LEDS_RED);
     etimer_set(&timer_wait, CLOCK_SECOND * 3);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
   }
@@ -77,13 +78,38 @@ AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
 /* Exercise 1c: alter the program such that the node prints "Hello World" infinitely to the screen,
  * with the delay of 4 seconds in between each statement
  */
+  // while (1)
+  //   {
+  //     printf("Hello, world\n");
+  //     etimer_set(&timer_wait, CLOCK_SECOND * 4);
+  //     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+  //   }
+
 
 /* Exercise 1d: alter the program such that the node stops printing "Hello World" after 10 times
  */
 
+// static int counter = 0;
+// while (counter < 10)
+// {
+//     printf("Hello, world\n");
+//     counter++;
+//     etimer_set(&timer_wait, CLOCK_SECOND * 4);
+//     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+// }
+
 /* Exercise 1e: alter the program such that the node turns on the blue led in each loop,
  * keeps it turned on for 1 second and then continues.
- */
+//  */
+//  while (1)
+//   {
+//     leds_on(LEDS_RED);
+//     etimer_set(&timer_blink, CLOCK_SECOND * 1);
+//     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_blink));
+//     leds_off(LEDS_RED);
+//   }
+
+// Cooja doesn't support blue LED but if it does, it would look like this.I used the red LED for testing.
 
 /* Exercise 1f: separate the led blinking logic from the "Hello World"
  * printing logic: use two different processes, one that a) infinitely turns on and off the blue
@@ -92,4 +118,35 @@ AUTOSTART_PROCESSES(&led_blink_process, &hello_world_process);
  *
  * The two processes shall be started when the node boots. The first (a) shall start immediately,
  * the second (b) shall start 30s after bootup.
+ while (1)
+  {
+    leds_on(LEDS_BLUE);  // Turn on the blue LED
+    etimer_set(&timer_blink, CLOCK_SECOND * 1);  // Keep it on for 1 second
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_blink));
+    leds_off(LEDS_BLUE);  // Turn off the LED
+
+    etimer_set(&timer_wait, CLOCK_SECOND * 3);  // Wait for 3 seconds (repeat interval = 4s)
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));
+  }
+
+  PROCESS_END();
+}
+
+PROCESS_THREAD(hello_world_process, ev, data)
+{
+  PROCESS_BEGIN();
+  static struct etimer timer_startup, timer_wait;
+
+  etimer_set(&timer_startup, CLOCK_SECOND * 30);  // Wait 30 seconds before starting
+  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_startup));
+
+  while (1)
+  {
+    printf("Hello, world\n");
+    etimer_set(&timer_wait, CLOCK_SECOND * 10);  // Set a 10-second interval
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer_wait));  // Wait until the timer expires
+  }
+
+  still used LEDS_RED for testing
+  Simulation already done with the given code
  */
--- exercises/item02/item02.c
+++ exercises/item02/item02.c
@@ -1,27 +1,25 @@
+Answered Exercise Items 1 - 5
+a95251e
+exercises\item02\item02.c
 
-/* Philipp Hurni, University of Bern, December 2013     */
-/* Modified for Zolertia Firefly - M. Cabilo, Apr 2019  */
-
+@@ -5,8 +5,9 @@
 #include <stdio.h> /* For printf() */
 #include <string.h>
 #include "dev/button-sensor.h" /* for the button sensor */
 #include "dev/zoul-sensors.h"  /* for the temperature sensor */
 #include "node-id.h"           /* get the variable node_id that holds the own node id */
+#include "arch/dev/sensor/sht11/sht11-sensor.h"
+//#include "dev/zoul-sensors.h"  /* for the temperature sensor */
+#include "sys/node-id.h"           /* get the variable node_id that holds the own node id */
 #include "dev/leds.h"
 /*---------------------------------------------------------------------------*/
 PROCESS(button_press_process, "Button Press Process");
-/*---------------------------------------------------------------------------*/
 
-void print_temperature_int_to_float(uint16_t temp)
-{
-  printf("%u.%uC\n", temp / 1000, temp % 1000);
-}
-
-PROCESS_THREAD(button_press_process, ev, data)
-{
+@@ -22,21 +23,25 @@ PROCESS_THREAD(button_press_process, ev, data)
   PROCESS_BEGIN();
 
   SENSORS_ACTIVATE(button_sensor);
+  SENSORS_ACTIVATE(sht11_sensor);
 
   while (1)
   {
@@ -29,31 +27,14 @@ PROCESS_THREAD(button_press_process, ev, data)
 
     leds_on(LEDS_BLUE);
 
+    leds_on(LEDS_RED);
+    printf("button pressed");
     printf("Temperature: ");
     SENSORS_ACTIVATE(cc2538_temp_sensor);
     print_temperature_int_to_float(cc2538_temp_sensor.value(CC2538_SENSORS_VALUE_TYPE_CONVERTED));
-    SENSORS_DEACTIVATE(cc2538_temp_sensor);
+    SENSORS_DEACTIVATE(cc2538_temp_sensor); 
+    print_temperature_int_to_float(sht11_sensor.value(SHT11_SENSOR_TEMP));
 
     leds_off(LEDS_BLUE);
+    leds_off(LEDS_RED);
   }
-
-  PROCESS_END();
-}
-
-/*---------------------------------------------------------------------------*/
-AUTOSTART_PROCESSES(&button_press_process);
-/*---------------------------------------------------------------------------*/
-
-/* Exercise 2a: compile and run the program. press the button, observe what it does with
- * the serial interface
- */
-
-/* Exercise 2b: alter the program such that whenever the button is pressed, the led
- * blinks and the string "button pressed" is printed
- */
-
-/*
- * Exercise 2c: read out the temperature from the temperature
- * sensor when the button is pressed. print the temperature to the serial interface by
- * passing the value read from the sensor to print_temperature_int_to_float().
- */
